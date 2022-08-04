ac0c11054812ddd8c7ea0d7c54781fbc6756d714
krakovmuffin <joseph@adriel.solutions>
2022-08-04 04:01:54 +0200

    Added default table and values for app settings

---
+++ setup/database/migration-08042022.sql
@@ -0,0 +1,31 @@
+-- settings
+CREATE TABLE settings (
+    pk INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    value TEXT NULL DEFAULT NULL
+);
+
+-- default settings
+INSERT INTO settings
+( name , type , value )
+VALUES
+-- smtp-related
+( 'SMTP_ENABLED' , 'boolean' , 'FALSE' ),
+( 'SMTP_HOST' , 'string' , NULL ),
+( 'SMTP_PORT' , 'int' , NULL ),
+( 'SMTP_USER' , 'string' , NULL ),
+( 'SMTP_PASS' , 'string' , NULL ),
+( 'SMTP_SECURITY' , 'string' , NULL ),
+( 'SMTP_FROM' , 'string' , NULL ),
+-- i18n-related
+( 'DEFAULT_LANGUAGE' , 'string' , 'en-us' ),
+-- storage-related
+( 'STORAGE_TYPE' , 'string' , 'local' ),
+( 'STORAGE_AWS_REGION' , 'string' , NULL ),
+( 'STORAGE_AWS_BUCKET' , 'string' , NULL ),
+( 'STORAGE_AWS_KEY' , 'string' , NULL ),
+( 'STORAGE_AWS_SECRET' , 'string' , NULL );
+
+
+-- users
