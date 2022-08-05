faee03d26b70abd805f35b487b5fbd2bae23f16b
krakovmuffin <joseph@adriel.solutions>
2022-08-05 19:35:29 +0200

    Added default redirect on frnt root url

--- app/routes/front.php
+++ app/routes/front.php
@@ -1,4 +1,11 @@
 <?php
     class R_Front extends Router {
-        protected function load() {  }
+        protected function load() {
+            $this->mount(new R_Front_Login());
+
+            // Default redirect
+            $this->use(function($req, $res) {
+                $res->redirect(front_path('/sign-in'));
+            });
+        }
     }
