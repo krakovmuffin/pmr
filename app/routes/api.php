b65f2b70d02a2b6fab53725ddf3a57c6b53e9935
krakovmuffin <joseph@adriel.solutions>
2022-08-06 01:10:33 +0200

    Added default error handler to all API routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,12 +1,36 @@
 <?php
     class R_Api extends Router {
         protected function load() {
+            /**
+             * Add support for :
+             * - HTTP default security good practice
+             * - CORS for JS XHR
+             * - JSON + Form user input decoding
+             * - Anti-request tampering
+             */
             $this->use(native_mdw('security'));
             $this->use(native_mdw('cors'));
             $this->use(native_mdw('json'));
             $this->use(native_mdw('form'));
             $this->use(native_mdw('signature'));
 
+            /**
+             * Mount app's routers
+             */
             $this->mount(new R_Api_Login());
+
+            /**
+             * Setup default errors handler
+             */
+            $this->set_error_handler(function($req, $res, &$next, $err) {
+                $content = [];
+
+                if(Options::get('MODE') === 'DEBUG')
+                    $content['error'] = strval($err);
+
+                $res->send_error([ 'content' => $content ]);
+
+                $next = false;
+            });
         }
     }
