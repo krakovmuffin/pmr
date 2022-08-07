f325eff194129274474c2f16644436a1b6d6bc0e
krakovmuffin <joseph@adriel.solutions>
2022-08-07 13:44:47 +0200

    Added proper check to reset-password API routes before enabling them

--- app/routes/api/login.php
+++ app/routes/api/login.php
@@ -4,8 +4,11 @@
             $controller = new C_Api_Login();
 
             $this->post('/sign-in', [$controller, 'authenticate']);
-            $this->post('/request-password-reset', [$controller, 'send_otp']);
-            $this->post('/verify-otp', [$controller, 'verify_otp']);
-            $this->post('/reset-password', [$controller, 'reset_password']);
+
+            if(Options::get('PASSWORD_RESET_ENABLED')) {
+                $this->post('/request-password-reset', [$controller, 'send_otp']);
+                $this->post('/verify-otp', [$controller, 'verify_otp']);
+                $this->post('/reset-password', [$controller, 'reset_password']);
+            }
         }
     }
