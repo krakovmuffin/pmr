36353831963bce4d29bdc234a485fe7994231251
krakovmuffin <joseph@adriel.solutions>
2022-08-11 08:10:37 +0200

    Fixed wording

--- app/views/partials/settingssidebar.php
+++ app/views/partials/settingssidebar.php
@@ -16,7 +16,7 @@
             'label' => __('Language'),
             'link' => front_path('/dashboard/settings/language'),
             'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.75"> <path stroke-linecap="round" stroke-linejoin="round" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" /> </svg>',
-            'description' => __("Define the app's global preferred language to translate the whole interface by default")
+            'description' => __("Define the app's default language to translate the whole interface for everyone")
         ],
         [
             'label' => __('Storage'),
@@ -28,7 +28,7 @@
             'label' => __('About'),
             'link' => front_path('/dashboard/settings/about'),
             'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.75"> <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /> </svg>',
-            'description' => __("Get precise information about your current instance and all the settings for it")
+            'description' => __("Get precise information about your current instance and all the settings of it")
         ]
 
 
