94c7f7d89fb73e8a68ef470acb1703d64de12a50
krakovmuffin <joseph@adriel.solutions>
2022-08-12 21:43:05 +0200

    Fixed a typo

--- app/views/emails/settings/test.php
+++ app/views/emails/settings/test.php
@@ -9,7 +9,7 @@
 
         <p><?= __("This is just an email to make sure that your settings are correct.") ?></p>
 
-        <p><?= __("If you're reading this, everything should be hine!") ?></p>
+        <p><?= __("If you're reading this, everything should be fine!") ?></p>
 
         <p><?= __("Enjoy your stay,") ?></p>
 
