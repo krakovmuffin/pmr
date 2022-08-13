a528b7c0cce7d0a049143c82db780f2bad2b7e4d
krakovmuffin <joseph@adriel.solutions>
2022-08-14 00:33:34 +0200

    Fixed color

--- app/views/components/textarea.php
+++ app/views/components/textarea.php
@@ -21,7 +21,7 @@
             <?php if( isset($params['disabled']) && $params['disabled'] === true ): ?>
                 class="resize-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm bg-gray-100"
             <?php else: ?>
-                class="resize-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                class="resize-none appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
             <?php endif; ?>
 
             placeholder="<?= empty($params['placeholder']) ? ($params['label'] ?? '') : $params['placeholder'] ?>"
