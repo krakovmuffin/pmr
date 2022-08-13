41439da54f3605d27771d4ab365dfff2e7fb1ae9
krakovmuffin <joseph@adriel.solutions>
2022-08-13 16:25:36 +0200

    Added component to go back in multi-step forms

---
+++ app/views/components/backstepbutton.php
@@ -0,0 +1,9 @@
+<button
+    type="button"
+    class="hover:text-slate-600 hover:bg-slate-100 focus:ring-slate-500 bg-slate-50 text-slate-500 border-slate-200 w-12 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed"
+    x-on:click="step = step - 1"
+>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+    </svg>
+</button>
