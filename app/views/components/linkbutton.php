7255989cd536d4730020c354a9d67be58fad7aaa
krakovmuffin <joseph@adriel.solutions>
2022-08-14 00:33:47 +0200

    Added support for inline SVG icon

--- app/views/components/linkbutton.php
+++ app/views/components/linkbutton.php
@@ -3,12 +3,18 @@
      * Parameters :
      * - href : string
      * - text : string
+     * - icon : string
      */
 ?>
 <a
     href="<?= $params['href'] ?>"
-    class="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 w-full"
+    class="inline-flex items-center justify-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 w-full"
 >
+    <?php if ( isset($params['icon']) ): ?>
+        <div class="-ml-1 mr-2">
+            <?= $params['icon'] ?>
+        </div>
+    <?php endif; ?>
     <?= $params['text'] ?>
 </a>
 
