a3d32ae9eeb8a6ea71ef882e3b8a34a649368f81
krakovmuffin <joseph@adriel.solutions>
2022-08-14 00:35:31 +0200

    Added support for initial / toggled text

--- app/views/components/togglebutton.php
+++ app/views/components/togglebutton.php
@@ -9,7 +9,7 @@
     type="button"
     class="inline-flex items-center justify-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 w-full"
     x-on:click="<?= $params['toggle'] ?> = !<?= $params['toggle'] ?>"
+    x-text="!<?= $params['toggle'] ?> ? '<?= $params['text_initial'] ?> ' : '<?= $params['text_toggle'] ?>'"
 >
-    <?= $params['text'] ?>
 </button>
 
